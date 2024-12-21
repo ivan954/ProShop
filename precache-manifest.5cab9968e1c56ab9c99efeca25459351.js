f54ab11189641ae709ae3d5d119b7b30439af3c2
ivan954 <78029556+ivan954@users.noreply.github.com>
2024-12-21 15:11:42 +0200

    Updates

---
+++ precache-manifest.5cab9968e1c56ab9c99efeca25459351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525fe1497501a973f5493de60e64fb7a",
+    "url": "/ProShop/index.html"
+  },
+  {
+    "revision": "26db0f4b1a911167d00c",
+    "url": "/ProShop/static/css/main.61c6d28d.chunk.css"
+  },
+  {
+    "revision": "30906def8549d54a3725",
+    "url": "/ProShop/static/js/2.2f14a119.chunk.js"
+  },
+  {
+    "revision": "ed32161e110b99fd71e65bc6a28901c8",
+    "url": "/ProShop/static/js/2.2f14a119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26db0f4b1a911167d00c",
+    "url": "/ProShop/static/js/main.334767ed.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/ProShop/static/js/main.334767ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93054878da1c0925fc27",
+    "url": "/ProShop/static/js/runtime-main.5aede0ea.js"
+  }
+]);
