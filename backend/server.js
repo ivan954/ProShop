488d29030aeaff9a9ef727b92b9762a64ecfec5c
ivan954 <78029556+ivan954@users.noreply.github.com>
2026-01-09 16:39:48 +0200

    Update server configuration: adjust static file paths for uploads and frontend build to ensure correct directory structure in production environment, and enhance CORS settings to include dynamic frontend URL.

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,8 @@ app.use(
       "https://ivan954.github.io",
       "http://localhost:3000",
       "https://ivan954.github.io/ProShop",
-    ],
+      process.env.FRONTEND_URL,
+    ].filter(Boolean),
     credentials: true,
   })
 );
@@ -44,13 +45,15 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => {
